7bb7e0589f5287cde1e3ed752a4148ba6037a863
gitFellpz <felipe.adrilima@gmail.com>
2023-04-25 09:15:16 -0300

    Exercicio 1 concluido!

--- src/exe1/Ex1.java
+++ src/exe1/Ex1.java
@@ -11,6 +11,19 @@ public class Ex1 {
 		
 		fila.init();
 		
+		System.out.print("Informe o valor: ");
+		int valor = input.nextInt();
+		
+		while(valor >= 0) {
+			fila.enqueue(valor);
+			System.out.print("Informe valor positivo(negativo para encerrar): ");
+			
+			valor = input.nextInt();
+		}
+		
+		while(!fila.isEmpty()) {
+			System.out.println("Valor retirado: " + fila.dequeue());
+		}
 		
 		input.close();
 	}
